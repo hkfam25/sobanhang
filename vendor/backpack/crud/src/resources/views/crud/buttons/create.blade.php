ea201823ba64b8afc005448b44b9d59a964df4d6
Momotarou2k <longchau121020@gmail.com>
2025-06-19 18:00:24 +0700

    cap nhat giao dien Dashboard

---
+++ vendor/backpack/crud/src/resources/views/crud/buttons/create.blade.php
@@ -0,0 +1,5 @@
+@if ($crud->hasAccess('create'))
+    <a href="{{ url($crud->route.'/create') }}" class="btn btn-primary" bp-button="create" data-style="zoom-in">
+        <i class="la la-plus"></i> <span>{{ 'Thêm' }}</span>
+    </a>
+@endif
